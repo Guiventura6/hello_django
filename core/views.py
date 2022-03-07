281b0e4f3add008b031ca3a6d5fbc8d7120142de
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-03-07 00:47:47 -0300

    criacao de testes de rotas

--- core/views.py
+++ core/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    result = num1 + num2
+    return HttpResponse('<h1>Soma</h1>'
+                        '<h2>{} + {} = {}</h2>'.format(num1, num2, result))
+
+def multiplicacao(request, num1, num2):
+    result = num1 * num2
+    return HttpResponse('<h1>Multiplicacao</h1>'
+                        '<h2>{} * {} = {}</h2>'.format(num1, num2, result))
+
+def divisao(request, num1, num2):
+    result = num1 / num2
+    return HttpResponse('<h1>Divisao</h1>'
+                        '<h2>{} / {} = {}</h2>'.format(num1, num2, result))
+
+def subtracao(request, num1, num2):
+    result = num1 - num2
+    return HttpResponse('<h1>Subtracao</h1>'
+                        '<h2>{} - {} = {}</h2>'.format(num1, num2, result))
